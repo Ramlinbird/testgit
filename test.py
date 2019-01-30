0ac3fcdac1849731f5246785df6d36b5d93909d3
Ramlinbird <ramlinbird@gmail.com>
2019-01-30 09:36:48 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+## @Author: pengyiyu01(pengyiyu01@baidu.com)
+## @Created: 2018/07/18 18:58:38 (Wed)
+## @Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+import re
+import time
+import copy
+import sys
+import glob
+import logging
+from httplib2 import Http
+from BeautifulSoup import BeautifulSoup
+
+def get_page(url, outfile=None):
+    """ GET url and save result-html to outfile """
+    req = Http(timeout=5)
+    headers  = {
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24',
+        'Content-type': 'application/x-www-form-urlencoded',
+        'Accept': '*/*',
+        'Accept-Charset': 'UTF-8,*;q=0.5',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    }
+    try:
+        rsp, content = req.request(url, "GET", headers=headers)
+        if outfile is not None:
+            with open(outfile, "w") as outf:
+                outf.write(content)
+    except:
+        print("Get %s error!" % url)
+    soup = BeautifulSoup(content)
+    parse_soup_v1(soup)
+
+def parse_soup_v1(soup):
+    print(soup.title.text.encode("utf-8"))
+    for link  in soup.findAll(name='img',attrs={"src":re.compile(r'^http')}):
+        print link.get('src')
+
+if __name__ == "__main__":
+    get_page("https://www.wikiart.org/en/grandma-moses/apple-butter-making-1947", "./test.html")
